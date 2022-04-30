355c5832f99ae4010652709db3c7f17ac0a6d32e
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization
    Inspired by dotOS
    
    [neobuddy89]
    Clean up unused code and convert to 1-100 scale.
    
    Change-Id: If1359191bbf1628143bde028bc947e3765f513fe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -32,8 +32,10 @@ import android.annotation.IntDef;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.provider.Settings;
 import android.os.Handler;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.util.Log;
 import android.util.MathUtils;
 import android.util.Pair;
@@ -190,6 +192,8 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
      * This should not be lower than 0.54, otherwise we won't pass GAR.
      */
     public static final float BUSY_SCRIM_ALPHA = 1f;
+
+    public static float mCustomScrimAlpha = 1f;
 
     /**
      * Scrim opacity that can have text on top.
@@ -410,6 +414,12 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         behindScrim.enableBottomEdgeConcave(mClipsQsScrim);
         mNotificationsScrim.enableRoundedCorners(true);
 
+        if (mScrimBehind != null) {
+            mCustomScrimAlpha = (Settings.System.getFloatForUser(
+                mScrimBehind.getContext().getContentResolver(),
+                Settings.System.QS_TRANSPARENCY, 100,
+                UserHandle.USER_CURRENT) / 100);
+        }
         final ScrimState[] states = ScrimState.values();
         for (int i = 0; i < states.length; i++) {
             states[i].init(mScrimInFront, mScrimBehind, mDozeParameters, mDockManager);
@@ -956,8 +966,8 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                 } else if (mClipsQsScrim) {
                     float behindFraction = getInterpolatedFraction();
                     behindFraction = (float) Math.pow(behindFraction, 0.8f);
-                    mBehindAlpha = 1;
-                    mNotificationsAlpha = behindFraction * mDefaultScrimAlpha;
+                    mBehindAlpha = mCustomScrimAlpha;
+                    mNotificationsAlpha = behindFraction * mCustomScrimAlpha;
                 } else {
                     mBehindAlpha = mPanelExpansionFraction * mCustomScrimAlpha;
                     mNotificationsAlpha =
@@ -1008,7 +1018,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             } else if (mClipsQsScrim) {
                 mNotificationsAlpha = behindAlpha;
                 mNotificationsTint = behindTint;
-                mBehindAlpha = 1;
+                mBehindAlpha = mCustomScrimAlpha;
                 mBehindTint = Color.TRANSPARENT;
             } else {
                 mBehindAlpha = behindAlpha;
@@ -1066,7 +1076,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         float behindAlpha;
         int behindTint = state.getBehindTint();
         if (mDarkenWhileDragging) {
-            behindAlpha = MathUtils.lerp(mDefaultScrimAlpha, stateBehind,
+            behindAlpha = MathUtils.lerp(mCustomScrimAlpha, stateBehind,
                     interpolatedFract);
         } else {
             behindAlpha = MathUtils.lerp(0 /* start */, stateBehind,
@@ -1082,7 +1092,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             }
         }
         if (mQsExpansion > 0) {
-            behindAlpha = MathUtils.lerp(behindAlpha, mDefaultScrimAlpha, mQsExpansion);
+            behindAlpha = MathUtils.lerp(behindAlpha, mCustomScrimAlpha, mQsExpansion);
             float tintProgress = mQsExpansion;
             if (mStatusBarKeyguardViewManager.isPrimaryBouncerInTransit()) {
                 // this is case of - on lockscreen - going from expanded QS to bouncer.
