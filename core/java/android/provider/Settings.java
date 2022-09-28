844d91a0db90ecf21fdf5c0ffb488a0bd1aa122e
SamarV-121 <samarvispute121@pm.me>
2022-09-28 08:57:10 +0000

    base: Add support for disabling fingerprint lockout [1/2]
    Change-Id: I687152fa0abe1f31fb1b283f3eb667f30bf81481
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7340,6 +7340,12 @@ public final class Settings {
         }
 
         /**
+         * Enable/Disable fingerprint lockout
+         * @hide
+         */
+        public static final String FINGERPRINT_LOCKOUT = "fingerprint_lockout";
+
+        /**
          * When to use Wi-Fi calling
          *
          * @see android.telephony.TelephonyManager.WifiCallingChoices
