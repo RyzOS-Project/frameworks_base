310dd3f92eff55907b5756f3ac836b50f67b2f91
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-29 17:05:16 +0800

    SystemUI: Introduce Notification Peek Display feature [1/2]
    * inspired from motorola lockscreen bubble style and android 15/16 beta minimalism feature
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSImpl.java
@@ -784,6 +784,11 @@ public class QSImpl implements QS, CommandQueue.Callbacks, StatusBarStateControl
         } else {
             MediaArtUtils.getInstance(mRootView.getContext()).updateMediaArtVisibility();   
         }
+        if (fullyCollapsed) {
+            com.android.systemui.notifications.ui.PeekDisplayViewController.Companion.getInstance().showPeekDisplayView();
+        } else {
+            com.android.systemui.notifications.ui.PeekDisplayViewController.Companion.getInstance().hidePeekDisplayView();
+        }
     }
 
     private void setAlphaAnimationProgress(float progress) {
