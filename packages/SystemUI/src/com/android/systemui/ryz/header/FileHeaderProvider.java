crdroidandroid/android_frameworks_base

SystemUI: Compress custom file headers properly
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/crdroid/header/FileHeaderProvider.java
@@ -37,6 +37,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 
+import com.android.internal.util.crdroid.ImageHelper;
 import com.android.systemui.R;
 
 public class FileHeaderProvider implements
@@ -114,7 +115,7 @@ public class FileHeaderProvider implements
         File file = new File(mContext.getFilesDir(), HEADER_FILE_NAME);
         if (file.exists()) {
             if (DEBUG) Log.i(TAG, "Load header image");
-            final Bitmap image = BitmapFactory.decodeFile(file.getAbsolutePath());
+            final Bitmap image = ImageHelper.getCompressedBitmap(file.getAbsolutePath());
             mImage = new BitmapDrawable(mContext.getResources(), image);
         }
     }

RyzOS-Project/frameworks_base

Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

packages/SystemUI/src/com/android/systemui/ryz/header/FileHeaderProvider.java
@@ -37,6 +37,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 
+import com.android.internal.util.ryz.ImageHelper;
 import com.android.systemui.res.R;
 
 public class FileHeaderProvider implements
@@ -114,7 +115,7 @@ public class FileHeaderProvider implements
         File file = new File(mContext.getFilesDir(), HEADER_FILE_NAME);
         if (file.exists()) {
             if (DEBUG) Log.i(TAG, "Load header image");
-            final Bitmap image = BitmapFactory.decodeFile(file.getAbsolutePath());
+            final Bitmap image = ImageHelper.getCompressedBitmap(file.getAbsolutePath());
             mImage = new BitmapDrawable(mContext.getResources(), image);
         }
     }